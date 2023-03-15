e9b41a5f0c25c1c3d1bcacb6aa1bee39b873428f
peppersora <110614387+peppersora@users.noreply.github.com>
2023-03-15 23:53:54 +0900

    commit

--- js/clock.js
+++ js/clock.js
@@ -1,49 +1,11 @@
-/*
-const clock=document.querySelector("#clock");
+// 현재시간 기능
+const clock = document.querySelector("#time");
 
-function sayHello(){
-    console.log("hello");
+function getClock() {
+  const date = new Date();
+  const hours = String(date.getHours()).padStart("2", 0);
+  const minutes = String(date.getMinutes()).padStart("2", 0);
+  clock.innerText = `${hours} : ${minutes}`;
 }
-interval: 매번 일어나야하는 무언가
-    예를 들어 2초마다 무슨일이 일어나게 하고
-    싶다 => 이때 쓰는게 interval!!
-    setInterval(sayHello,5000); 5초마다 실행시킴
-    setTimeout(sayHello,5000);  새로고침을하고 5초뒤에 실행됨
-*/
-
-/* padstrat() 사용법
-    "string".padstart(자릿수,추가되는 숫자 또는 문자);
-   1. "1".padstart("2",0); =>"01"
-   2. "Hello".padend("10",x); => "Helloxxxxx"
-*/
-
-
-
- //현재시간 시계만들기 => 매순간 new date object를 만들고 있다..!
-const clock=document.querySelector(".header_clock");
-console.log(clock);
-function getClock(){
-    const date = new Date();
-    const hours =String(date.getHours()).padStart("2",0);
-    const minutes = String(date.getMinutes()).padStart("2",0);
-    const seconds = String(date.getSeconds()).padStart("2",0);
-    clock.innerText = `${hours} : ${minutes} : ${seconds}`;
-}
-  getClock();
-// 존재 유무에 따라 getclock이 한번실행된상태에서 function인 getclock가 1초마다 실행되게 해준것.!
-setInterval(getClock,1000);
-
-
-// 오늘 날짜 넣기...
-const day = document.querySelector(".header_date");
-
-const date = new Date();
-const year =date.getFullYear();
-const month = date.getMonth()+1;
-const today = date.getDate();
-day.innerText = `${year} / ${month} / ${today}`;
-
-
-
-
-
+getClock();
+setInterval(getClock, 1000);
--- js/weather.js
+++ js/weather.js
@@ -1,53 +1,52 @@
 //1. geolocation함수를 사용할 것이고, 이 함수는 위치의 좌표를 알려준다.
 // my api : 4d72cd42b82357e8222fd40062dd758a
 
-    const API_KEY = "4d72cd42b82357e8222fd40062dd758a";
+const API_KEY = "4d72cd42b82357e8222fd40062dd758a";
 
-function onGeoOk(position){
-    console.log(position);
-    const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
-    fetch(url)
+function onGeoOk(position) {
+  console.log(position);
+  const lat = position.coords.latitude;
+  const lng = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+  fetch(url)
     .then((Response) => Response.json())
-    .then((data) =>{ 
-      const weather = document.querySelector("#header_weather span:first-child");
+    .then((data) => {
+      const weather = document.querySelector(
+        "#header_weather span:first-child"
+      );
       const city = document.querySelector("#header_weather span:last-child");
-      city.innerText= data.name;
+      city.innerText = data.name;
       weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
-    })
-    //fetch는 promise인데 =>promise는 당장 뭔가 일어나지 않고 시간이 좀 걸린뒤에 일어나는 일 =>then사용
-    // console.log("you live in",lat,lng);
+    });
+  //fetch는 promise인데 =>promise는 당장 뭔가 일어나지 않고 시간이 좀 걸린뒤에 일어나는 일 =>then사용
+  // console.log("you live in",lat,lng);
 }
-function onGeoError(){
-alert("can't find you. No weather for you.")
+function onGeoError() {
+  alert("can't find you. No weather for you.");
 }
 //getcurrent(successCallback,errorCallback)두개의 인자를 사용
-navigator.geolocation.getCurrentPosition(onGeoOk,onGeoError);
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
 
 /*2. lat,lng를 이용해 알게 된 좌표(숫자)를 장소로 바꿔줄 서비스를 사용해야 한다.
     기본적으로 api는 다른 서버와 이야기 할 수 있는 방법*/
-
 
 // bgm 넣기
 
 let audio = document.querySelector("#audio");
 let button = document.querySelector("#music");
 
-
-
-function musicPlay(){
-  audio.loop=true;
-  button.style.color="red";
+function musicPlay() {
+  audio.loop = true;
+  button.style.color = "red";
   audio.play();
 }
 
-button.addEventListener("click",musicPlay);
-button.addEventListener("dbclick",musicPause);
+button.addEventListener("click", musicPlay);
+button.addEventListener("dbclick", musicPause);
 
-function musicPause(){
+function musicPause() {
   console.log("clicked");
-  button.style.color="black";
+  button.style.color = "black";
   audio.pause();
 }
 //search 기능
@@ -56,11 +55,7 @@ var input = document.getElementById("search");
 input.addEventListener("keyup", function (event) {
   if (window.event.keyCode === 13) {
     // event.preventDefault();
-    let url = "https://www.google.com/search?q="+input.value;
+    let url = "https://www.google.com/search?q=" + input.value;
     window.open(url);
-
   }
 });
-
-
-
